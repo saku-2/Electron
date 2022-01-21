013d374c97eb72611984f7ed8f8d97faf46328f1
津田由紀子 <yukiko.tsuda.1026@gmail.com>
2022-01-21 13:51:48 +0900

    「テキスト」音声出力 : https://qiita.com/taiko1/items/240eea6eb597701f83bb

--- src/index.js
+++ src/index.js
@@ -18,7 +18,8 @@ const createWindow = () => {
   mainWindow.loadFile(path.join(__dirname, 'index.html'));
 
   // Open the DevTools.
-  mainWindow.webContents.openDevTools();
+  // mainWindow.webContents.openDevTools();
+  mainWindow.webContents.openDevTools({ mode: 'detach' });
 };
 
 // This method will be called when Electron has finished
